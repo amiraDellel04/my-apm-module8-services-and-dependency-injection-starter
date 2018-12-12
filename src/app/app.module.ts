f9f6f0e32325a8a77a64ead3cd514477f79e9462
Amira Dellel <amira.dellel@sofrecom.com>
2018-12-12 15:14:01 +0100

    Navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import {RouterModule, Route} from '@angular/router';
 
 import {HttpClientModule} from '@angular/common/http';
 
@@ -10,19 +11,39 @@ import { ProductsListComponent } from './components/products-list/products-list.
 import { ProductsFilterPipe } from './pipes/products-filter.pipe';
 import { StarComponent } from './components/shared/star/star.component';
 import { HttpClient } from 'selenium-webdriver/http';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { NotfoundComponent } from './components/notfound/notfound.component';
+import { WelcomeComponent } from './components/welcome/welcome.component';
+
+const routes: Route[] = [
+
+{path: 'list', component : ProductsListComponent},
+{path: 'welcome', component : WelcomeComponent},
+{path: '', redirectTo : 'welcome' , pathMatch: 'full'},
+{path: '**', component : NotfoundComponent}
+
+
+
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductsListComponent,
     ProductsFilterPipe,
-    StarComponent
+    StarComponent,
+    NavbarComponent,
+    FooterComponent,
+    NotfoundComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
     NgbModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
